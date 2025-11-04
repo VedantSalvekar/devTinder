ff90fb837a04fd3dac2032dcd4071674d1d3bcad
Vedant Salvekar <vedantsalvekar86@gmail.com>
2025-11-04 17:41:19 +0000

    temp db key

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://vedantsalvekar86_db_user:BY7afnVQ5NS8OBG4@learnnode.4wchioh.mongodb.net/devTinder"
+    "mongodb+srv://vedantsalvekar86_db_user:HBjTx1TCEzVfE1sw@learnnode.4wchioh.mongodb.net/devTinder"
   );
 };
 
